2cb7fed61f340e09a3a7c20050e5ed33ee68606a
mikhail izotov <157229582+mikkoBB@users.noreply.github.com>
2024-04-11 19:25:02 +0300

    all new

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 class Category:
+    '''
+    Класс, описывающий категорию товаров, описание названий категорий, свойств и список товаров
+    '''
     title: str
     description: str
     products: list
@@ -6,6 +9,9 @@ class Category:
     all_unique_prod_quantity = 0
 
     def __init__(self, title, description, products):
+        '''
+        Метод инициализации атрибутов класса
+        '''
         self.title = title
         self.description = description
         self.products = products
@@ -16,12 +22,18 @@ Category.all_unique_prod_quantity += 1
 
 
 class Product:
+    '''
+    Класс, описывающий название товара, его описание, также цену и количество
+    '''
     name: str
     description: str
     price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
+        '''
+        Метод инициализации атрибутов класса Продукт
+        '''
         self.name = name
         self.description = description
         self.price = float(price)
--- test.py
+++ test.py
@@ -3,21 +3,36 @@ from main import Category, Product
 
 @pytest.fixture
 def category():
+    '''
+    Тестирование класса Категория
+    '''
     return Category('name_1', 'text_1', [])
 
 def test_category_init(category):
+    '''
+    Тестирование инициализации атрибутов класса
+    '''
     assert category.title == 'name_1'
     assert category.description == 'text_1'
     assert category.products == []
 def test_category_count(category):
+    '''
+    Тестирование подсчетов уникальных товаров и количества категорий товаров
+    '''
     assert category.all_unique_prod_quantity == 1
     assert category.all_category_quantity == 1
 
 @pytest.fixture
 def product():
+    '''
+    Тестирование класса Продукт
+    '''
     return Product("a", "x", 10.0,2)
 
 def test_product_init(product):
+    '''
+    Тестирование инициализации названий товаров, их описаний, цен и количества
+    '''
     assert product.name == "a"
     assert product.description == "x"
     assert product.price == 10.0
