0f7cdd7bb9521d6953780400d8f068b6cd79a0be
mikhail izotov <mikko.izoto@gmail.com>
2024-04-19 01:11:29 +0300

    new

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-class Category:
-    '''
-    Класс, описывающий категорию товаров, описание названий категорий, свойств и список товаров
-    '''
-    title: str
-    description: str
-    products: list
-    all_category_quantity = 0
-    all_unique_prod_quantity = 0
-
-    def __init__(self, title, description, products):
-        '''
-        Метод инициализации атрибутов класса
-        '''
-        self.title = title
-        self.description = description
-        self.products = products
-        Category.all_category_quantity += 1
-        Category.all_unique_prod_quantity += len(set(self.products))
-
-
-
-
-
 class Product:
     '''
     Класс, описывающий название товара, его описание, также цену и количество
@@ -31,7 +7,8 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+
+    def __init__(self, name, description, price,quantity):
         '''
         Метод инициализации атрибутов класса Продукт
         '''
@@ -39,8 +16,30 @@ class Product:
         self.description = description
         self.price = float(price)
         self.quantity = quantity
+p1 = Product('a', 'x', 10.0, 2)
+p2 = Product('a1', 'x1', 10.4, 6)
+list_products = [p1, p2]
 
 
+class Category:
+    '''
+    Класс, описывающий категорию товаров, описание названий категорий, свойств и список товаров
+    '''
+    title: str
+    description: str
+    products: list
+    all_category_quantity = 0
+    all_unique_prod_quantity = 0
+    all_category_quantity += 1
+    all_unique_prod_quanti += len(set(list_products))
+
+    def __init__(self, title, description, list_products):
+        '''
+        Метод инициализации атрибутов класса
+        '''
+        self.title = title
+        self.description = description
+        self.list_products = list_products
 
 
 
--- test_main.py
+++ test_main.py
@@ -1,15 +1,76 @@
 import pytest
-from main import Category
-from main import Product
+from main import Category, Product
 import os
-from datetime import datetime
+
+
+@pytest.fixture
+def product():
+    '''
+    Тестирование класса Продукт
+    '''
+    return Product('a', 'x', 10.0, 2)
+
+
+def test_product_init(product):
+    '''
+    Тестирование инициализации названий товаров, их описаний, цен и количества
+    '''
+    assert product.name == 'a'
+    assert product.description == 'x'
+    assert product.price == 10.0
+    assert product.quantity == 2
+
+
+def test_product(product):
+    '''
+    Тестирование класса Продукт
+    '''
+    assert product.name == 'a'
+    assert product.description == 'x'
+    assert product.price == 10.0
+    assert product.quantity == 2
+
+
+@pytest.fixture
+def product():
+    '''
+    Тестирование класса Продукт
+    '''
+    return Product('a1', 'x1', 10.4, 6)
+
+
+def test_product_init(product):
+    '''
+    Тестирование инициализации названий товаров, их описаний, цен и количества
+    '''
+    assert product.name == 'a1'
+    assert product.description == 'x1'
+    assert product.price == 10.4
+    assert product.quantity == 6
+
+
+def test_product(product):
+    '''
+    Тестирование класса Продукт
+    '''
+    assert product.name == 'a1'
+    assert product.description == 'x1'
+    assert product.price == 10.4
+    assert product.quantity == 6
+
+
+p1 = Product('a', 'x', 10.0, 2)
+p2 = Product('a1', 'x1', 10.4, 6)
+list_products = [p1, p2]
+
 
 @pytest.fixture
 def category():
     '''
     Тестирование класса Категория
     '''
-    return Category('name_1', 'text_1', [('a', 'x', 10.0, 2)])
+    return Category('name_1', 'text_1', list_products)
+
 
 def test_category_init(category):
     '''
@@ -17,37 +78,14 @@ def test_category_init(category):
     '''
     assert category.title == 'name_1'
     assert category.description == 'text_1'
-    assert category.products == [('a', 'x', 10.0, 2)]
+    assert category.list_products == list_products
+
+
 def test_category_count(category):
     '''
     Тестирование подсчетов уникальных товаров и количества категорий товаров
     '''
+    assert Category.all_category_quantity == 1
     assert Category.all_unique_prod_quantity == 2
-    assert Category.all_category_quantity == 2
-
-@pytest.fixture
-def product():
-    '''
-    Тестирование класса Продукт
-    '''
-    return Product("a", "x", 10.0,2)
-
-def test_product_init(product):
-    '''
-    Тестирование инициализации названий товаров, их описаний, цен и количества
-    '''
-    assert product.name == "a"
-    assert product.description == "x"
-    assert product.price == 10.0
-    assert product.quantity == 2
-
-
-def test_product(product):
-    assert product.name == "a"
-    assert product.description == "x"
-    assert product.price == 10.0
-    assert product.quantity == 2
-
-
 
 
